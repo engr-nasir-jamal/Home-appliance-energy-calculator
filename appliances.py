d952f8a94f3c7c6a394f5166eb5d290efe8f68d7
engr-nasir-jamal <engr_nasir@outlook.com>
2026-05-14 13:53:32 +0200

    Added Appliance class (OOP structure)

---
+++ appliances.py
@@ -0,0 +1,6 @@
+class Appliance:
+    def __init__(self, name, power_watts, hours_per_day):
+        self.name = name
+        self.power_watts = power_watts
+        self.hours_per_day = hours_per_day
+        
