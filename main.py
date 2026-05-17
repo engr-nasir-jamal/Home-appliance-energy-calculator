819349a33903d8c202ce5a41a590f9d3e0d6138f
engr-nasir-jamal <engr_nasir@outlook.com>
2026-05-17 23:59:40 +0200

    Added energy calculation logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-from appliances import Appliance
+from appliance import Appliance
 
 devices = []
 
@@ -11,7 +11,17 @@ for row in reader:
     power = float(row["power_watts"])
     hours = float(row["hours_per_day"])
 
-    appliances = Appliance(name, power, hours)
-    devices.append(appliances)
+    device = Appliance(name, power, hours)
+    devices.append(device)
 
 file.close()
+
+from calculator import daily_energy, monthly_energy, monthly_cost
+results = []
+
+for device in devices:
+    daily = daily_energy(device.power_watts, device.hours_per_day)
+    monthly = monthly_energy(daily)
+    cost = monthly_cost(monthly)
+
+    results.append((device.name, monthly, cost))
