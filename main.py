969e469ccb8e241a777212af53a9ef4605a2ade8
engr-nasir-jamal <engr_nasir@outlook.com>
2026-05-18 22:06:07 +0200

    Added user selection, quantity input and energy calculation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
 from appliance import Appliance
-from calculator import daily_energy, monthly_energy, monthly_cost
 
-# Load appliances from CSV
+# Appliances from CSV
 appliance_list = []
 
 file = open("data.csv", "r")
@@ -40,6 +39,8 @@ for i in range(n):
     selected_devices.append((device, quantity, hours))
 
 # Calculations
+from calculator import daily_energy, monthly_energy, monthly_cost
+
 results = []
 
 for item in selected_devices:
@@ -54,44 +55,6 @@ for item in selected_devices:
     results.append((device.name, monthly, cost))
 
 print("\nEnergy Report")
-print("----------------")
-
-for result in results:
-    print("Device Name:", result[0])
-    print("Monthly Energy:", round(result[1], 2), "kWh")
-    print("Monthly Cost:", round(result[2], 2), "Euro")
-    print("----------------")
-
-from calculator import daily_energy, monthly_energy, monthly_cost
-results = []
-
-for device in devices:
-    daily = daily_energy(device.power_watts, device.hours_per_day)
-    monthly = monthly_energy(daily)
-    cost = monthly_cost(monthly)
-
-    results.append((device.name, monthly, cost))
-
-print("Energy Report")
-print("----------------")
-
-for result in results:
-    print("Device Name:", result[0])
-    print("Monthly Energy:", round(result[1], 2), "kWh")
-    print("Monthly Cost:", round(result[2], 2), "Euro")
-    print("----------------")
-
-from calculator import daily_energy, monthly_energy, monthly_cost
-results = []
-
-for device in devices:
-    daily = daily_energy(device.power_watts, device.hours_per_day)
-    monthly = monthly_energy(daily)
-    cost = monthly_cost(monthly)
-
-    results.append((device.name, monthly, cost))
-
-print("Energy Report")
 print("----------------")
 
 for result in results:
